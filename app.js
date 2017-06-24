d1e10563489b1a74a94b7ecb2bb14e628107d04b
Stefan Thon <smuemd@gmail.com>
2017-06-25 00:05:11 +0200

    Update App.js   - Use normal Css instead of Sss   - add css_modules folder to postcss path Refactor Css   - refactor _global.sss and index.sss   - add css_modules folder   - add test module

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const htmlStandards = require('reshape-standard')
 const cssStandards = require('spike-css-standards')
 const jsStandards = require('spike-js-standards')
 const pageId = require('spike-page-id')
+const path = require('path')
 const env = process.env.NODE_ENV
 
 module.exports = {
   devtool: 'source-map',
-  matchers: { html: '*(**/)*.sgr', css: '*(**/)*.sss' },
+  matchers: { html: '*(**/)*.sgr', css: '*(**/)*.css' },
   ignore: ['**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
   reshape: htmlStandards({
     locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } },
     minify: env === 'production'
   }),
   postcss: cssStandards({
+    parser: false,
+    path: path.join(__dirname, './assets/css/css_modules'),
     minify: env === 'production',
     warnForDuplicates: env !== 'production'
   }),
