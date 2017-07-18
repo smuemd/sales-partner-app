f9387e4d4301f3a8d9e0cfa5d29c5b7b52e97b3c
Loganathan Sellappa <Loganathan.Sellappa@logmein.com>
2017-07-18 21:58:20 +0530

    AccountModel replaced with mithril routes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,56 +1,10 @@
 'use strict'
+
 import m from 'mithril'
+import HomePage from './views/homePage'
+import User from './model/user'
 
-/**
- * StromDAO-businessObject Example
- */
-
-// Account model
-const Account = {
-  ledger: '0xfcBfE1e0D5B62b4E281609B150423b0F15082319',
-  id: '0x0022aFE423D59a9f4AC99927a5d6D7E7c4BA017c',
-  soll: 0,
-  haben: 0,
-  saldo: () => {
-    return Account.haben - Account.soll
-  },
-
-  getLedger: () => document.node.stromkonto(Account.ledger),
-
-  getHaben: () => Account.getLedger().then(ledger => {
-    return ledger.balancesHaben(Account.id).then(haben => {
-      Account.haben = haben
-      return Account.haben
-    })
-  }),
-
-  getSoll: () => Account.getLedger().then(ledger => {
-    return ledger.balancesSoll(Account.id).then(soll => {
-      Account.soll = soll
-      return Account.soll
-    })
-  }),
-
-  getSaldo: () => Promise.all([
-    Account.getHaben(),
-    Account.getSoll()
-  ]).then(values => {
-    return Account.saldo()
-  })
-}
-
-// Applying Model methods
-Account.getSaldo().then(res => {
-  let obj = {
-    account: Account.id,
-    soll: Account.soll,
-    haben: Account.haben,
-    saldo: Account.saldo
-  }
-  console.log(obj)
-
-  /**
-   * Mithril example
-   */
-  m.render(document.querySelector('span.mithril-test'), 'hello world from mithril (' + Account.saldo() + ')')
+User.checkLogin()
+m.route(document.querySelector('body'), '/', {
+  '/': HomePage
 })
