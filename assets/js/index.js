6baa8ea42ef89aba6d0f6304db3211b64b1432f1
Stefan Thon <smuemd@gmail.com>
2017-07-19 13:59:18 +0200

    add mithril routing example I

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,184 +2,99 @@
 import m from 'mithril'
 
 /**
+ * Routing Strategy
  *
- * StromDAO-businessObject Example
- *
+ * represent changes in the route as changes in the model...
+ * ...then continue as before by using that model to render a view.
  */
 
-// Create Instance with external ID 1234
-let node = new document.StromDAOBO.Node({
-  external_id: '19810930',
-  testMode: true,
-  rpc: 'https://demo.stromdao.de/rpc',
-  abilocation:
-    'https://cdn.rawgit.com/energychain/StromDAO-BusinessObject/6dc9e073/smart_contracts/'
-})
-
-document.node = node
-
-// Account model
-const Account = {
-  ledger: '0xfcBfE1e0D5B62b4E281609B150423b0F15082319',
-  id: '0x0022aFE423D59a9f4AC99927a5d6D7E7c4BA017c',
-  soll: 0,
-  haben: 0,
-  saldo: () => {
-    return Account.haben - Account.soll
-  },
-
-  getLedger: () => document.node.stromkonto(Account.ledger),
-
-  getHaben: () =>
-    Account.getLedger().then(ledger => {
-      return ledger.balancesHaben(Account.id).then(haben => {
-        Account.haben = haben
-        return Account.haben
-      })
-    }),
-
-  getSoll: () =>
-    Account.getLedger().then(ledger => {
-      return ledger.balancesSoll(Account.id).then(soll => {
-        Account.soll = soll
-        return Account.soll
-      })
-    }),
-
-  getSaldo: () =>
-    Promise.all([Account.getHaben(), Account.getSoll()]).then(() => {
-      return Account.saldo()
-    })
+// Settings object with routes "/fruits" and "/vegetables" .
+const settings = {
+  paths : ['fruits', 'vegetables']
 }
 
-// Applying Model methods
-/* TODO: this is not working right now. @Thorsten. */
-Account.getSaldo().then(() => {
-  console.log(node.rpcprovider)
-  let obj = {
-    account: Account.id,
-    soll: Account.soll,
-    haben: Account.haben,
-    saldo: Account.saldo
-  }
-  console.log(obj)
-})
+window.addEventListener("DOMContentLoaded", main)
 
-/**
- *
- * Mithril app structure example
- * via https://github.com/pakx/the-mithril-diaries
- *
- */
+function main (event) {
+  console.log('DOM fully loaded and parsed ', event)
 
-/**
- * (1) Single Application-level Model
- *
- * => a single "model" to track 2 bits of data:
- *    (a) the message to display, and
- *    (b) the number of times the button is clicked.
- */
+  // (1) set up an initial model and pass settings
+  const model = createModel(settings)
+  // (2) create actions object
+  const actions = createActions(model)
+  // (3) set up a route resolver and pass model and actions objects
+  const routeResolver = createRouteResolver(model, actions)
+  const defaultRoute = '/' + settings.paths[0]
 
-function createModel () {
+  m.route(document.body, defaultRoute, routeResolver)
+}
+
+function createModel (settings) {
   return {
-    clickCount: 0,
-    msg: 'Hello, word from Mithril'
+    paths: settings.paths.slice(),
+    routeName: undefined
   }
 }
-
-/**
- * (2) Consolidated App Actions
- *
- * => extract/consolidate application behavior/logic into a single entity.
- * => pass in the model, so actions can operate on passed in data rather than reaching out.
- */
 
 function createActions (model) {
   return {
-    onButtonClick: onButtonClick
+    onNavigateTo: onNavigateTo
   }
 
-  function onButtonClick () {
-    model.clickCount += 28
-    model.msg = model.msg.toUpperCase()
+  function onNavigateTo (routeName, params) {
+    model.routeName = routeName
   }
 }
 
-/**
- *
- * (3) Single single `view()` function broken out into modular sub-views.
- *
- * => encapsulate modularity in an IIFE (note the return view toward its end)
- * => pass that app-model into the view function, so view operates on passed-in data
- *    rather than reach out and access a global variable
- * => TODO: use ES6 or CommonJS modules in favor of IIFE
- */
+// use m.route() and a RouteResolver, in routeResolver.onmatch (which we can consider "outside input")
+// call a method of the actions object (onNavigateTo)
+// the actions object will update model ... which will then be rendered by view()
+function createRouteResolver (model, actions) {
+  return model.paths.reduce(function (acc, item) {
+    acc['/' + item] = {
+      onmatch: function (params, route) {
+        actions.onNavigateTo(item, params)
+      },
+      render: function () {
+        return view(model, actions)
+      }
+    }
+    return acc
+  }, {})
+}
 
+// create a view app which will display a "FRUITS" or "VEGETABLES" pane
+// depending on whether we're on a "/fruits" or "/vegetables" route.
 const view = (function () {
   function view (model, actions) {
     return viewApp(model, actions)
   }
 
   function viewApp (model, actions) {
-    return {
-      view: function () {
-        return m('.app', [viewHeader(), viewBody(model, actions), viewFooter()])
-      }
-    }
-  }
-
-  function viewHeader () {
-    return m('.header', '(Headerrrr)')
-  }
-
-  function viewBody (model, actions) {
-    return m('.body', [
-      m('.message', model.msg),
-      m('.clickCount', model.clickCount),
-      m('button.btn', { onclick: actions.onButtonClick }, 'click me')
+    return m('.app', [
+      viewHeader(model),
+      model.routeName === 'fruits' ? viewFruits(model) : viewVegetables(model)
     ])
   }
 
-  function viewFooter () {
-    return m('.footer', '(Fooooter)')
+  function viewHeader (model) {
+    return m('.nav',
+      model.paths.map(function (item) {
+        return [
+          m('a', { href: '/' + item, oncreate: m.route.link }, item),
+          ' '
+        ]
+      })
+    )
   }
 
-  return view // <-- the only "exported" function
+  function viewFruits (model) {
+    return m('.fruits', 'FRUITS')
+  }
+
+  function viewVegetables (model) {
+    return m('.vegetables', 'VEGETABLES')
+  }
+
+  return view
 })()
-
-/**
- * Putting It All Together
- *
- * (1) set up an initial model
- * (2) create an actions object
- * (3) then pass model and actions objects
- *
- *  I choose to use a main() function, which itself is called from
- *  window.addEventListener("DOMContentLoaded", ...). YMMV.
- */
-
-window.addEventListener("DOMContentLoaded", main)
-
-function main (event) {
-  console.log('DOM fully loaded and parsed ', event)
-  // (1) create actions object to pass into actions and view on mount.
-  let model = createModel()
-  // (2) create actions object to pass into the view on mount.
-  let actions = createActions(model)
-  // (3) mount the view() function with Mithril's m.mount(); pass in model and actions objects
-  m.mount(document.body, view(model, actions))
-}
-
-/**
- *
- * Unidirectional Data Flow
- *
- * NOTE: Notice the sequence of events, and the resulting unidirectional data flow:
- * 1) we start with a model
- * 2) based on that model we render a view
- * 3) user interactions with the view -- and more generally any outside input -- is
- *    sent to an actions object
- * 4) based on that input the actions object updates the model
- *
- * ... and we're back to step 1!
- */
