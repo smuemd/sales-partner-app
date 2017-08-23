74b7b05a801986ffd10a9fdab660e53b679db557
Stefan Thon <smuemd@gmail.com>
2017-08-23 23:33:53 +0200

    Refactor index.js  - remove mithril test code

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,55 +2,9 @@
 import m from 'mithril'
 
 /**
- * StromDAO-businessObject Example
- */
-
-// Account model
-const Account = {
-  ledger: '0xfcBfE1e0D5B62b4E281609B150423b0F15082319',
-  id: '0x0022aFE423D59a9f4AC99927a5d6D7E7c4BA017c',
-  soll: 0,
-  haben: 0,
-  saldo: () => {
-    return Account.haben - Account.soll
-  },
-
-  getLedger: () => document.node.stromkonto(Account.ledger),
-
-  getHaben: () => Account.getLedger().then(ledger => {
-    return ledger.balancesHaben(Account.id).then(haben => {
-      Account.haben = haben
-      return Account.haben
-    })
-  }),
-
-  getSoll: () => Account.getLedger().then(ledger => {
-    return ledger.balancesSoll(Account.id).then(soll => {
-      Account.soll = soll
-      return Account.soll
-    })
-  }),
-
-  getSaldo: () => Promise.all([
-    Account.getHaben(),
-    Account.getSoll()
-  ]).then(values => {
-    return Account.saldo()
-  })
-}
-
-// Applying Model methods
-Account.getSaldo().then(res => {
-  let obj = {
-    account: Account.id,
-    soll: Account.soll,
-    haben: Account.haben,
-    saldo: Account.saldo
-  }
-  console.log(obj)
-
-  /**
-   * Mithril example
-   */
-  m.render(document.querySelector('span.mithril-test'), 'hello world from mithril (' + Account.saldo() + ')')
-})
+  * Mithril example
+  */
+m.render(
+  document.querySelector('#app'),
+  'hello world from mithril ( + Account.saldo() + )'
+)
