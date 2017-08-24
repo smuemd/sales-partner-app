6a4524784456706243b8c3cebb8b7638c4dd8120
Stefan Thon <smuemd@gmail.com>
2017-08-24 03:00:31 +0200

    Add front end app structure  - wiring actions model view together

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,24 @@
 'use strict'
 import m from 'mithril'
+import settings from './settings'
+import createModel from './model'
+import createDataAPI from './dataAPI'
+import createActions from './actions'
+import createView from './view'
+import createRouteResolver from './routes'
 
-/**
-  * Mithril example
-  */
-m.render(
-  document.querySelector('#app'),
-  'hello world from mithril ( + Account.saldo() + )'
-)
+console.log('you can use ES6 here : )')
+
+window.addEventListener('DOMContentLoaded', main())
+
+function main () {
+  const model = createModel(settings)
+  const dataAPI = createDataAPI(settings)
+  const actions = createActions(model, dataAPI)
+  const view = createView() // vwApp(model, actions)
+  const routeResolver = createRouteResolver(model, actions, view)
+  const defaultRoute = settings.defaultRoute
+
+  m.route.prefix(settings.routePrefix)
+  m.route(document.body, defaultRoute, routeResolver) // m.route(document.querySelector('main'), defaultRoute, routeResolver)
+}
