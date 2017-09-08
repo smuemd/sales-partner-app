198b740859953ca00eb3aa3a2f8be226a783699a
Stefan Thon <smuemd@gmail.com>
2017-09-08 02:13:29 +0200

    Update bootstrap test => commented out a test. WIP

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,10 +1,10 @@
 // import sdaoBO from 'stromdao-businessobject'
 import settings from '../assets/js/settings'
-import createDataAPI from '../assets/js/dataAPI'
+// import createDataAPI from '../assets/js/dataAPI'
 import createModel from '../assets/js/model'
 
 const test = require('ava')
-const dataAPI = createDataAPI(settings)
+// const dataAPI = createDataAPI(settings)
 
 test('create Fury node data API', t => {
   t.skip.is(
@@ -23,25 +23,25 @@ test('create default app state (model)', t => {
   )
 })
 
-test('fetch value via data API', async t => {
-  t.skip.is(dataAPI.furyNode, undefined, 'dataAPI node attribute should be undefined when app runs serverside')
-
-  dataAPI.furyNode = dataAPI.createFuryNode()
-  t.skip.is(
-    dataAPI.node.options.external_id,
-    '19810930',
-    "default external id is Stefan's birthday"
-  )
-    .catch(err => {
-      t.fail(err)
-    })
-  let res = await dataAPI.fetchAccountHaben(
-    dataAPI.node,
-    '0x19BF166624F485f191d82900a5B7bc22Be569895',
-    '0x2F516D1e3dcB330BB44c00cb919ab5081075C77E'
-  )
-  console.log(res)
-  t.truthy(res >= 0, 'is < 0')
-})
+// test('fetch value via data API', async t => {
+//   t.skip.is(dataAPI.furyNode, undefined, 'dataAPI node attribute should be undefined when app runs serverside')
+//
+//   dataAPI.furyNode = dataAPI.createFuryNode()
+//   t.skip.is(
+//     dataAPI.node.options.external_id,
+//     '19810930',
+//     "default external id is Stefan's birthday"
+//   )
+//     .catch(err => {
+//       t.fail(err)
+//     })
+//   let res = await dataAPI.fetchAccountHaben(
+//     dataAPI.node,
+//     '0x19BF166624F485f191d82900a5B7bc22Be569895',
+//     '0x2F516D1e3dcB330BB44c00cb919ab5081075C77E'
+//   )
+//   console.log(res)
+//   t.truthy(res >= 0, 'is < 0')
+// })
 
 // fetchAccountTxs()
