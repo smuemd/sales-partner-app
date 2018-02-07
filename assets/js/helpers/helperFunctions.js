133430b25574d626b718e171535f6c2898ec08f4
Stefan Thon <smuemd@gmail.com>
2018-02-07 22:35:06 +0100

    update helper

--- assets/js/helpers/helperFunctions.js
+++ assets/js/helpers/helperFunctions.js
@@ -1,3 +1,3 @@
 export const decodeBase16 = value => parseInt(value, 16)
 export const getFormattedAddress = value => value.substr(0, 10)
-export const getEuro = value => (value / 10000000000000).toFixed(2)
+export const getEuro = value => (value / 10000000).toFixed(2)
