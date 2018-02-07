858a3796ac0a808fffc9d6e1c1b6b5794b7c4813
Stefan Thon <smuemd@gmail.com>
2018-02-07 21:52:21 +0100

    test new ledger address

--- assets/js/settings/index.js
+++ assets/js/settings/index.js
@@ -41,7 +41,7 @@ const routesDescription = [
 const ethAddresses = {
   mp: '0x83F8B15eb816284ddcF2ff005Db7a19196d86ae1', // Meter point address
   smpc: '0x2F516D1e3dcB330BB44c00cb919ab5081075C77E', // Account address: single meter point clearing
-  sk: '0x19BF166624F485f191d82900a5B7bc22Be569895' // Ledger address (stromkonto)
+  sk: '0x5b0c9c6c9fa239455293147ABcD525075B686643' // Ledger address (stromkonto)
 }
 
 /** settings export */
