0688558a18ac75296464b3cf55fe048a9b41271e
Stefan Thon <smuemd@gmail.com>
2018-02-07 22:29:39 +0100

    yet another one

--- assets/js/actions/index.js
+++ assets/js/actions/index.js
@@ -50,8 +50,8 @@ function onNavigateTo (model) {
 
 function fetchAccountData (accountAddress) {
   let node = dataApi.furyNode
-  let ledger = '0x5b0c9c6c9fa239455293147ABcD525075B686643' // TODO derive form settings
-  let account = '0x19c541b0832543faed2c42ec8d6b96d9287dc33f'
+  let ledger = '0x691C0173bbAF9B8e8293D6d6b145bbb775B1A84e' // TODO derive form settings
+  let account = '0x19c541b0832543FaEd2C42EC8d6B96D9287DC33f'
     // accountAddress ||
     //window.localStorage.getItem(
     //  'user[' + model.user.extId + ']:remoteNode:address'
